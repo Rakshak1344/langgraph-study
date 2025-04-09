ecd0a2314233fbda392ef71d67279a2f3b8b0fc0
Rakshak1344 <dev.rakshith14@gmail.com>
2025-04-09 09:13:18 +0530

    chore: reviser chain.

--- src/reflexion_agent/main.py
+++ src/reflexion_agent/main.py
@@ -2,6 +2,7 @@ import datetime
 from dotenv import load_dotenv
 
 from schemas import AnswerQuestion
+from src.reflexion_agent.schemas import ReviseAnswer
 
 load_dotenv()
 from langchain_core.output_parsers.openai_tools import (
@@ -16,6 +17,7 @@ llm = ChatOpenAI(model="gpt-4-turbo-preview")
 parser = JsonOutputToolsParser(return_id=True)
 parser_pydantic = PydanticToolsParser(tools=[AnswerQuestion])
 
+# Phase 1
 actor_prompt_template = ChatPromptTemplate.from_messages(
     [
         (
@@ -41,6 +43,20 @@ first_responder_prompt_template = actor_prompt_template.partial(
 first_responder = first_responder_prompt_template | llm.bind_tools(
     tools=[AnswerQuestion], tool_choice="AnswerQuestion"
 )
+
+# Phase 2
+revise_instructions = """Revise your previous answer using the new information.
+    - You should use the previous critique to add important information to your answer.
+    - You MUST include numerical citations in your revised answer to ensure it can be verified.
+    - Add a "References" section to the bottom of your answer (which does not count towards the word limit). In form of:
+        - [1] https://example.com
+        - [2] https://example.com
+    - You should use the previous critique to remove superfluous information from your answer and make SURE it is not more than 250 words.
+"""
+
+reviser = actor_prompt_template.partial(
+    first_instruction=revise_instructions
+) | llm.bind_tools(tools=[ReviseAnswer], tool_choice="ReviseAnswer")
 
 if __name__ == "__main__":
     human_message = HumanMessage(
--- src/reflexion_agent/schemas.py
+++ src/reflexion_agent/schemas.py
@@ -17,3 +17,10 @@ class AnswerQuestion(BaseModel):
         ...,
         description="1-3 search queries for researching improvements to address the critique of your current answer."
     )
+
+class ReviseAnswer(AnswerQuestion):
+    """Revise your original answer to your question."""
+
+    references: List[str] = Field(
+        description="citations motivating your updated answer."
+    )
