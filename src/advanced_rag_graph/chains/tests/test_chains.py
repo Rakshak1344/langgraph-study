38c39650d1ea1a3394f65e47f87522f1a63e1c3c
Rakshak1344 <dev.rakshith14@gmail.com>
2025-04-14 23:52:05 +0530

    chore: test case.

--- src/advanced_rag_graph/chains/tests/test_chains.py
+++ src/advanced_rag_graph/chains/tests/test_chains.py
@@ -1,2 +1,24 @@
-def test_chain() -> None:
-    assert 1 == 1
+from src.advanced_rag_graph.chains.retrival_grader_chain import GradeDocuments, retrieval_grader
+from src.advanced_rag_graph.ingestion import retriever
+
+
+def test_retrieval_grader_answer_yes() -> None:
+    question = "agent memory"
+    docs = retriever.invoke(question)
+    doc_txt = docs[0].page_content
+
+    res: GradeDocuments = retrieval_grader.invoke(
+        {"question": question, "document": doc_txt}
+    )
+    assert res.binary_score == "yes"
+
+
+def test_retrieval_grader_answer_no() -> None:
+    question = "agent memory"
+    docs = retriever.invoke(question)
+    doc_txt = docs[0].page_content
+
+    res: GradeDocuments = retrieval_grader.invoke(
+        {"question": "How to make pizza", "document": doc_txt}
+    )
+    assert res.binary_score == "no"
