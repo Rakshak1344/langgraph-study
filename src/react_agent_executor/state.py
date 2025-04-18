5026a7166568b62e292ef19d03af2b9e3711de66
Rakshak1344 <dev.rakshith14@gmail.com>
2025-04-18 19:21:10 +0530

    chore: reflection agent.

--- src/react_agent_executor/state.py
+++
@@ -1,9 +0,0 @@
-import operator
-from typing import TypedDict, Union, Annotated
-from langchain_core.agents import AgentAction, AgentFinish
-
-
-class AgentState(TypedDict):
-    input: str
-    agent_outcome: Union[AgentAction, AgentFinish, None]
-    intermediate_steps: Annotated[list[tuple[AgentAction, str]], operator.add]
